526f9255333e7dcc2fe84ecc120a50cd13176849
Babu Saheb <babusaheb@Babu-Saheb-MacBook-Air.local>
2026-03-07 21:29:05 +0530

    Basic module for revision: learning how to create APIs for POST, GET, UPDATE, and DELETE operations

--- project-01/index.js
+++ project-01/index.js
@@ -1,9 +1,14 @@
-const express =require('express');
-
+const express = require('express');
+const fs = require('fs');
 const users = require('./MOCK_DATA.json');
 
 const app = express();
 const PORT = 8000;
+/// middlwware -pluging
+app.use(express.urlencoded({ extended: false }))
+// Middlewares
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // ROUTS..
 app.get("/", (req, res) => {
@@ -37,9 +42,15 @@ app.get("/api/users", (req, res) => {
 // });
 
 app.post("/api/users", (req, res) => {
-// TODO: we will implement this later
-/// CREATE A NEW USERS
-return res.json({status: "panding"});
+    /// CREATE A NEW USERS
+    const body = req.body;
+    console.log("body", body);
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+        return res.json({ status: "panding", id: users.length + 1 });
+
+    })
+
 });
 
 
@@ -49,20 +60,34 @@ return res.json({status: "panding"});
 // return res.json({status: "panding"});
 // });
 
-app.route("/api/users/:id").get("/api/users/:id", (req, res) =>{
+app.route("/api/users/:id").get((req, res) => {
     const id = Number(req.params.id);
-    const user =users.find(user => user.id === id);
+    const user = users.find(user => user.id === id);
     return res.json(user);
-    
-}).get((req, res) => {
-    // TODO: we will implement this later
-/// Edit users with id.
-return res.json({status: "panding"});
 
 }).patch((req, res) => {
+    /// Edit users with id.
+
+    const id = Number(req.params.id);
+    const body = req.body;
+
+    const user = users.find(user => user.id === id);
+
+    if (!user) {
+        return res.json({ status: "User not found" });
+    }
+
+    // Update only provided fields
+    Object.assign(user, body);
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+        return res.json({ status: "panding", updatedUser: user });
+    });
+    
+}).delete((req, res) => {
     // TODO: we will implement this later
-/// delete users
-return res.json({status: "panding"});
+    /// delete users
+    return res.json({ status: "panding" });
 });
 
 
@@ -74,4 +99,4 @@ return res.json({status: "panding"});
 // });
 
 
-app.listen(PORT, () => console.log(`server started on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`server started on PORT http://localhost:${PORT}`));
